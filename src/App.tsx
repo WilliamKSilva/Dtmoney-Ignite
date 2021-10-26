c852f2091b55c82bf512d00d6f5df39a6b91bccf
Williamkds <wilz.kelvin12@gmail.com>
2021-10-26 17:19:18 -0300

    All the components is already done, now is time to start to transition from static content to dynamic.

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
+import { GlobalStyle } from './styles/global';
+
+export function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Dashboard />
+      <GlobalStyle />
+    </div>
+  );
+}
+
+
